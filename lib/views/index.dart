4efdbf2e5905d11202e03612262f44fda7dd3395
yogesh upadhyay <yogeshh2021@gmail.com>
2021-07-11 23:13:04 +0530

    index view updated

--- lib/views/index.dart
+++ lib/views/index.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:glassmorphism/glassmorphism.dart';
 
@@ -117,34 +118,37 @@ class _IndexState extends State<Index> {
                       SizedBox(width: 5.0,),
                       Column(
                         children: [
-                          GlassmorphicContainer(
-                            width: size.width*0.425,
-                            height: size.height*0.2,
-                            borderRadius: 20,
-                            blur: 20,
-                            alignment: Alignment.bottomCenter,
-                            border: 2,
-                            linearGradient: LinearGradient(
-                              begin: Alignment.topLeft,
-                              end: Alignment.bottomRight,
-                              colors: [
-                                Color(0xff1f727a).withOpacity(0.1),
-                                Color(0xff43c59e).withOpacity(0.05),
-                              ],
-                              stops: [
-                                0.1,
-                                1,
-                              ]
+                          GlassContainer(
+                            size: size,
+                            widthPercentage: 0.425,
+                            heightPercentage: 0.2,
+                            child: Container(
+                              margin: EdgeInsets.all(20.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    children: [
+                                      FaIcon(FontAwesomeIcons.piggyBank),
+                                      SizedBox(width: 5.0,),
+                                      Text(
+                                        "Savings",
+                                        style: GoogleFonts.montserratAlternates(
+                                          fontSize: 20
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(height: 8.0,),
+                                  Text(
+                                    "\u20B9 4,315",
+                                    style: GoogleFonts.montserratAlternates(
+                                      fontSize: 26
+                                    ),
+                                  )
+                                ],
+                              ),
                             ),
-                            borderGradient: LinearGradient(
-                              begin: Alignment.topLeft,
-                              end: Alignment.bottomRight,
-                              colors: [
-                                Color(0xFFffffff).withOpacity(0.5),
-                                Color((0xFFFFFFFF)).withOpacity(0.5),
-                              ],
-                            ),
-                            child: Text("Current Targets")
                           ),
                           SizedBox(height: 5.0,),
                           GlassmorphicContainer(
@@ -186,6 +190,52 @@ class _IndexState extends State<Index> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class GlassContainer extends StatelessWidget {
+  const GlassContainer({
+    Key? key,
+    required this.size, this.widthPercentage, this.heightPercentage, required this.child,
+  }) : super(key: key);
+
+  final Size size;
+  final widthPercentage;
+  final heightPercentage;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return GlassmorphicContainer(
+      padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
+      width: size.width*widthPercentage,
+      height: size.height*heightPercentage,
+      borderRadius: 45,
+      blur: 50,
+      alignment: Alignment.bottomCenter,
+      border: 2,
+      linearGradient: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [
+          Color(0xff1f727a).withOpacity(0.1),
+          Color(0xff43c59e).withOpacity(0.05),
+        ],
+        stops: [
+          0.1,
+          1,
+        ]
+      ),
+      borderGradient: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [
+          Color(0xFFffffff).withOpacity(0.5),
+          Color((0xFFFFFFFF)).withOpacity(0.5),
+        ],
+      ),
+      child: child
     );
   }
 }
